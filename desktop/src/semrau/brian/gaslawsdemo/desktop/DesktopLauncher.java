28dadca0ee3065a6ca20a2ff97efe756939d56c5
Brian Semrau <brian.semrau@gmail.com>
2016-06-01 09:13:49 -0400

    Created LibGDX project

---
+++ desktop/src/semrau/brian/gaslawsdemo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package semrau.brian.gaslawsdemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import semrau.brian.gaslawsdemo.GasLawsDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GasLawsDemo(), config);
+	}
+}
---
+++ html/src/semrau/brian/gaslawsdemo/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package semrau.brian.gaslawsdemo.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import semrau.brian.gaslawsdemo.GasLawsDemo;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new GasLawsDemo();
+        }
+}
